d807f82e8b68561907d2a31702a733685d027909
yjot2 <2010guabo@gmail.com>
2026-06-06 14:04:21 -0500

    Subida de codido de version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yjotdev.calculadordemateriales"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
         testInstrumentationRunner = "com.yjotdev.calculadordemateriales.CustomTestRunner"
     }
     signingConfigs {
