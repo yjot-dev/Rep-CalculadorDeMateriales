776adc1bfe3d8d28835c21894a884795ea9c9fd0
yjot2 <2010guabo@gmail.com>
2026-08-30 17:56:38 -0500

    Actualización de build.gradle.kts (app) y dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,16 +74,16 @@ dependencies {
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     //Hilt
     implementation(libs.dagger.hilt.android)
-    implementation(libs.androidx.activity)
     ksp(libs.dagger.hilt.android.compiler)
     //Test
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
-    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.dagger.hilt.android.testing)
     kspAndroidTest(libs.dagger.hilt.android.compiler)
